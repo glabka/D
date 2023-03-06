8ab5f05243e9f6b0b3efefc151f4f0bf2e4076ed
straca13 <straca13@gmail.com>
2023-03-06 22:07:01 +0100

    Delete tomas_sandbox directory

--- tomas_sandbox/pokusy/query_recipes_test.sql
+++
@@ -1,2 +0,0 @@
-SELECT recipe_name FROM recipes_list WHERE author='autor receptu';
-SELECT ingredient_name_r, weight_g FROM recipes_ingredients WHERE id_recipe_fk=(SELECT id_recipe FROM recipes_list WHERE author='autor receptu' AND recipe_name='jméno pokrmu');
--- tomas_sandbox/pomocne/run_sql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# run sql file (first parameter) in database specified by $OSD_DB
-mysql -u "$OSD_USERNAME" -D "$OSD_DB" --password="$OSD_PASSWORD" < "$1"
-echo "DONE"
